f42d26e9313cec40d42f3b91d3563417c96a8cdc
Mohammad Zubair Akrami <paghmane@yahoo.com>
2025-12-08 09:39:24 +0430

    Shadcn-vue is configured

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,20 +1,21 @@
+<script setup>
+import { Button } from "@components/ui/button";
+
+defineProps({
+    message: String
+});
+</script>
 <template>
     <div>
         <h1>Hello from Inertia + Vue 3</h1>
         <p>{{ message }}</p>
+
         <div class="bg-blue-500 text-white p-4 rounded">
             Tailwind is working!
         </div>
-        <div class="bg-red-600 text-white p-5 rounded">
-            Tailwind Test Box
-        </div>
+
+        <Button>Click me</Button>
     </div>
 </template>
 
-<script>
-export default {
-    props: {
-        message: String,
-    },
-};
-</script>
+
